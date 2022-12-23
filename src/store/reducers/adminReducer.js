67d0481a39cac5a610e650ae127b181f94f09308
Giang Pham <giang.pham1610@gmail.com>
2022-12-23 18:41:46 +0100

    load genders using Redux

--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -1,30 +1,30 @@
 import actionTypes from '../actions/actionTypes';
 
 const initialState = {
-    isLoggedIn: false,
-    adminInfo: null
+    genders: [],
+    roles: [],
+    positions: []
 }
 
 const adminReducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.ADMIN_LOGIN_SUCCESS:
+        case actionTypes.FETCH_GENDER_START:
             return {
                 ...state,
-                isLoggedIn: true,
-                adminInfo: action.adminInfo
             }
-        case actionTypes.ADMIN_LOGIN_FAIL:
+
+        case actionTypes.FETCH_GENDER_SUCCESS:
+            let copyState = {...state};
+            copyState.genders = action.data
+            return {
+                ...copyState,
+            }
+
+        case actionTypes.FETCH_GENDER_FAIL:
             return {
                 ...state,
-                isLoggedIn: false,
-                adminInfo: null
             }
-        case actionTypes.PROCESS_LOGOUT:
-            return {
-                ...state,
-                isLoggedIn: false,
-                adminInfo: null
-            }
+        
         default:
             return state;
     }
